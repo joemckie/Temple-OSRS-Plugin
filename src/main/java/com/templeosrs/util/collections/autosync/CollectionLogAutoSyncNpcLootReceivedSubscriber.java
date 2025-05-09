bc27613731932ec2a1ea5f0d894fba9b62265834
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-05-09 09:31:15 +0100

    chore: remove unused imports

--- src/main/java/com/templeosrs/util/collections/autosync/CollectionLogAutoSyncNpcLootReceivedSubscriber.java
+++ src/main/java/com/templeosrs/util/collections/autosync/CollectionLogAutoSyncNpcLootReceivedSubscriber.java
@@ -1,18 +1,12 @@
 package com.templeosrs.util.collections.autosync;
 
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.events.ChatMessage;
 import net.runelite.client.eventbus.EventBus;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.NpcLootReceived;
 import net.runelite.client.game.ItemManager;
 
 import javax.inject.Inject;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static net.runelite.client.util.Text.removeTags;
 
 @Slf4j
 public class CollectionLogAutoSyncNpcLootReceivedSubscriber
